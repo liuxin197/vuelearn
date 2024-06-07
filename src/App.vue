171b84a1d3c147c58112dff3663f5a03a1196022
liuxin197 <liuxin18602261021@outlook.com>
2024-06-07 11:37:31 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in list">{{ item }}</li>
+    </ul>
+    <button @click="add">添加</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive, proxyRefs } from "vue";
+// ref,reactive
+// ref 支持所有的类型 reactive 引用类型 Array Object Map Set
+// ref 取值 赋值 都需要加.value reactive 是不需要.value
+// reactive proxy 不能直接赋值 否则破坏响应式对象
+// 解决方案 数组可以使用push 加解构
+let list = reactive<string[]>([]);
+
+const add = () => {
+  setTimeout(() => {
+    let res = ["EDG", "RNG", "JDG"];
+      list.push(...res);
+    console.log(list);
+    
+  }, 2000);
+};
+// git test
+</script>
+<style scoped></style>
